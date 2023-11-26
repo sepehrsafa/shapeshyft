7ec8aab5f401f0a23ba1664894dd800b2a5b5daa
Parthraj Veluri <Parthrajveluri@yahoo.com>
2023-11-26 18:00:02 -0500

    working meal plan recommendation

--- app/services/predictions/meal_recommender/meal_recommendor.py
+++ app/services/predictions/meal_recommender/meal_recommendor.py
@@ -1,40 +1,77 @@
+import os
 import pandas as pd
-import joblib
 import json
 
-final_df = pd.read_csv("app/services/predictions/meal_recommender/preprocessed_mealplans.csv")
+try:
+    current_directory = os.path.dirname(os.path.abspath(__file__))
+except NameError:
+    current_directory = os.getcwd()
+
+try:
+    mealplans_path = os.path.join(current_directory, 'mealplans.csv')
+    dishes_path = os.path.join(current_directory, 'dishes.csv')
+
+    mealplans_df = pd.read_csv(mealplans_path)
+    dishes_df = pd.read_csv(dishes_path)
+    
+except Exception as e:
+    raise Exception(f"Error in file operations: {e}")
+
+try:
+    mealplans_columns_to_keep = ['id', 'calories', 'breakfast0', 'lunch0', 'snacks0', 'dinner0']
+    mealplans_df = mealplans_df[mealplans_columns_to_keep]
+    mealplans_df.dropna(subset=['breakfast0', 'lunch0', 'snacks0', 'dinner0'], inplace=True)
+
+    dishes_df['id'] = dishes_df['id'].astype(mealplans_df['breakfast0'].dtype)
+except Exception as e:
+    raise Exception(f"Error processing dataframes: {e}")
+
+try:
+    breakfast_df = mealplans_df.join(dishes_df.set_index('id'), on='breakfast0', rsuffix='_breakfast')
+    lunch_df = breakfast_df.join(dishes_df.set_index('id'), on='lunch0', rsuffix='_lunch')
+    dinner_df = lunch_df.join(dishes_df.set_index('id'), on='dinner0', rsuffix='_dinner')
+    final_df = dinner_df.join(dishes_df.set_index('id'), on='snacks0', rsuffix='_snacks')
+
+    final_columns_to_keep = ['id', 'calories', 'title', 'title_lunch', 'title_snacks', 'title_dinner']
+    final_df = final_df[final_columns_to_keep]
+    final_df.rename(columns={'title': 'breakfast', 'title_lunch': 'lunch', 'title_dinner': 'dinner', 'title_snacks': 'snacks'}, inplace=True)
+
+    final_df.to_csv('../preprocessed_mealplans.csv', index=False)
+except Exception as e:
+    raise Exception(f"Error joining dataframes: {e}")
 
 def recommend_meal(calorie_intake, tolerance=100):
-    
-    model = joblib.load('app/services/predictions/meal_recommender/meal_recommendation_model.pkl')
+    if calorie_intake < 500:
+        raise ValueError("Calorie intake must be at least 500 calories.")
 
-    possible_meals = final_df[(final_df['calories'] >= calorie_intake - tolerance) &
-                              (final_df['calories'] <= calorie_intake + tolerance)]
-
-    while possible_meals.empty:
-        tolerance += 50
+    try:
         possible_meals = final_df[(final_df['calories'] >= calorie_intake - tolerance) &
                                   (final_df['calories'] <= calorie_intake + tolerance)]
 
-    selected_plan = possible_meals.sample(n=1)
+        while possible_meals.empty:
+            tolerance += 50
+            possible_meals = final_df[(final_df['calories'] >= calorie_intake - tolerance) &
+                                      (final_df['calories'] <= calorie_intake + tolerance)]
 
-    breakfast_title = selected_plan['breakfast'].values[0]
-    lunch_title = selected_plan['lunch'].values[0]
-    dinner_title = selected_plan['dinner'].values[0]
-    snacks_title = selected_plan['snacks'].values[0]
+        selected_plan = possible_meals.sample(n=1)
 
-    recommendation = {
-        'calorie_intake': calorie_intake,
-        'meals': {
-            'breakfast': breakfast_title,
-            'lunch': lunch_title,
-            'snacks': snacks_title,
-            'dinner': dinner_title
+        breakfast_title = selected_plan['breakfast'].values[0]
+        lunch_title = selected_plan['lunch'].values[0]
+        dinner_title = selected_plan['dinner'].values[0]
+        snacks_title = selected_plan['snacks'].values[0]
+
+        recommendation = {
+            'calorie_intake': calorie_intake,
+            'meals': {
+                'breakfast': breakfast_title,
+                'lunch': lunch_title,
+                'snacks': snacks_title,
+                'dinner': dinner_title
+            }
         }
-    }
-    return recommendation
-    #replace return with the variable: "recommendation"
-    return {"calorie_intake": 100, "meals": {"breakfast": "a", "lunch":"b", "dinner":"c", "snacks":"d" }}
+        return recommendation
+    except Exception as e:
+        raise Exception(f"Error in meal recommendation: {e}")
 
-
-
+# Example of usage
+# print(recommend_meal(input_calorie))
